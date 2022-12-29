bdc1d05f475b6687b66a2ade1b2efcbe4b7ec294
Parija <parija.jamatia.pj@gmail.com>
2022-12-29 16:18:38 +0530

    custom response

--- request.js
+++ request.js
@@ -1,12 +1,32 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from Node.js </h1></body>');
-    res.write('</html>');
-    res.end();
+    const url = req.url;
+    if(url === '/home'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<body><h1>Welcome to home page</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
+    else if(url === '/about'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<body><h1>Welcome to About Us page</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
+    else if(url === '/node'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<body><h1>Welcome to my Node js project</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
+   
 
 })
 server.listen(3000);
